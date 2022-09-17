0852035b0b3e3582ac56426e81e5c5f1ec9d3fe2
fadillaoegi <fadilfdl2710@gmail.com>
2022-09-17 23:11:27 +0700

    Update Cendol

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: const Text("Haiii kamuu"),
+    );
   }
 }
